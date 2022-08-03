db2fbf1c76da27bc4fa741ca5e324682f5dbad4c
Burak Özen <28873023+thereturn932@users.noreply.github.com>
2022-08-03 13:56:07 +0300

    initial comit

--- contracts/GenNFT.sol
+++ contracts/GenNFT.sol
@@ -1,5 +1,3 @@
-pragma solidity ^0.8.0;
-
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -7,8 +5,9 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+import "hardhat/console.sol";
 
 interface IERC2981Royalties {
     function royaltyInfo(uint256 _tokenId, uint256 _value)
@@ -25,7 +24,7 @@ contract Gen30 is
     IERC2981Royalties
 {
     address royaltyOwner;
-    uint24 _royaltyAmount = 500;
+    uint24 _royaltyAmount;
     using Strings for uint256;
 
     string private _baseTokenURI;
@@ -37,14 +36,17 @@ contract Gen30 is
     uint public memberLimit = 89;
     uint public followerLimit = 244;
 
-    uint public memberMinted;
-    uint public followerMinted;
+    uint public memberMintedCount;
+    uint public followerMintedCount;
+    uint public totalMinted;
 
-    mapping (address => uint) totalMinted;
+    mapping(address => uint) userMinted;
+    mapping(address => bool) memberMint;
+    mapping(address => bool) followerMint;
 
+    bool public mintable; // false
     bool public inPreSale = true;
-    bool public mintable;
-    bool public claimable;
+    bool public claimable; // false
     uint public totalRevenue;
     uint public revenuePerToken;
 
@@ -52,20 +54,34 @@ contract Gen30 is
     mapping(uint => bool) isClaimed;
 
     uint maxSupply = 444;
+    uint ownerCanClaimAll = 2**256 - 1;
 
-    constructor(string memory baseTokenURI, uint256 _price)
-        ERC721("Gen3.0", "GEN3")
-    {
+    constructor(
+        string memory baseTokenURI,
+        uint256 _price,
+        bytes32 _rootMembers,
+        bytes32 _rootFollowers,
+        address _royaltyRecipient,
+        uint24 __royaltyAmount // 1000 is 10% 500 is 5%
+    ) ERC721("Gen3.0", "GEN3") {
+        rootMembers = _rootMembers;
+        rootFollowers = _rootFollowers;
         _baseTokenURI = baseTokenURI;
         price = _price;
+        royaltyOwner = _royaltyRecipient;
+        _royaltyAmount = __royaltyAmount;
     }
 
     function setPrice(uint256 _price) external onlyOwner {
         price = _price;
     }
 
-    function setRoot(bytes1 newRoot) external onlyOwner {
-        root = newRoot;
+    function setRootFollower(bytes1 _rootFollower) external onlyOwner {
+        rootFollowers = _rootFollower;
+    }
+
+    function setRootMembers(bytes1 _rootMembers) external onlyOwner {
+        rootMembers = _rootMembers;
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -80,40 +96,107 @@ contract Gen30 is
         mintable = !mintable;
     }
 
-    function mint(uint256[] calldata _tokenIds, bytes32[] _merkleProof) external payable {
+    function mint(uint256[] calldata _tokenIds, bytes32[] calldata _merkleProof)
+        external
+        payable
+    {
         require(mintable, "Mint hasn't started yet");
         if (inPreSale) {
-            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-            require(_tokenIds.length == 1, "Only 1 NFT can be minted during presale");
-            require(MerkleProof.verify(_merkleProof, rootMembers, leaf) || MerkleProof.verify(_merkleProof, rootFollowers, leaf),"You don't have whitelist");
-            if(MerkleProof.verify(_merkleProof, rootMembers, leaf)){
-                require(memberMinted < memberLimit, "All member NFTs are sold");
-                memberMinted++;
-            }
-            else if(MerkleProof.verify(_merkleProof, rootFollowers, leaf)){
-                require(followerMinted < followerLimit, "All follower NFTs are sold");
-                followerMinted++;
+            bytes32 leaf = bytes32(uint256(uint160(msg.sender)));
+            require(
+                _tokenIds.length == 1,
+                "Only 1 NFT can be minted during presale"
+            );
+            // console.log(
+            //     "Is member",
+            //     MerkleProof.verify(_merkleProof, rootMembers, leaf)
+            // );
+            // console.log(
+            //     "Is follower",
+            //     MerkleProof.verify(_merkleProof, rootFollowers, leaf)
+            // );
+            require(
+                MerkleProof.verify(_merkleProof, rootMembers, leaf) ||
+                    MerkleProof.verify(_merkleProof, rootFollowers, leaf),
+                "You don't have whitelist"
+            );
+            if (MerkleProof.verify(_merkleProof, rootMembers, leaf)) {
+                // console.log("MEMBER");
+                require(!memberMint[msg.sender], "Already minted as a member");
+                require(
+                    memberMintedCount < memberLimit,
+                    "All member NFTs are sold"
+                );
+                memberMintedCount++;
+                memberMint[msg.sender] = true;
+            } else if (MerkleProof.verify(_merkleProof, rootFollowers, leaf)) {
+                require(
+                    !followerMint[msg.sender],
+                    "Already minted as a follower"
+                );
+                require(
+                    followerMintedCount < followerLimit,
+                    "All follower NFTs are sold"
+                );
+                followerMintedCount++;
+                followerMint[msg.sender] = true;
             }
         }
         require(_tokenIds.length > 0, "Amount cannot be zero");
-        require(msg.value == price * _tokenIds.length, "Not enough balance");
-        require((totalMinted[msg.sender] + _tokenIds.length) <= 10, "Maximum 10 NFT per wallet");
+        if (msg.sender != owner()) {
+            require(
+                (totalMinted + _tokenIds.length) < 400,
+                "Only 400 tokens can be minted by users"
+            );
+            require(
+                (userMinted[msg.sender] + _tokenIds.length) <= 10,
+                "Maximum 10 NFT per wallet"
+            );
+            require(
+                msg.value == price * _tokenIds.length,
+                "Wrong amount of Ethers"
+            );
+        }
 
         for (uint256 i; i < _tokenIds.length; i++) {
+            require(_tokenIds[i] < 444, "ID of token can't be larger than 443");
             isSold[_tokenIds[i]] = true;
             _mint(msg.sender, _tokenIds[i]);
         }
-        totalMinted[msg.sender] += _tokenIds.length;
+        userMinted[msg.sender] += _tokenIds.length;
+        totalMinted += _tokenIds.length;
         totalRevenue += msg.value;
-        payable(owner()).transfer(msg.value);
+        (bool success, ) = owner().call{value: ((msg.value * 90) / 100)}("");
+        require(success, "Transfer failed.");
+        // console.log(userMinted[msg.sender]);
+        // console.log("Holds",msg.value == price * _tokenIds.length);
+        // console.log("Value",msg.value);
+        // console.log("MINTED");
     }
 
     function setPreSale() external onlyOwner {
         inPreSale = !inPreSale;
     }
 
+    function calculateClaimableShare()
+        external
+        view
+        returns (uint claimableShare)
+    {
+        uint count = balanceOf(msg.sender);
+        require(count > 0);
+        uint unClaimed;
+        for (uint i = 0; i < count; i++) {
+            uint tokenId = tokenOfOwnerByIndex(msg.sender, i);
+            if (!isClaimed[tokenId]) {
+                unClaimed++;
+            }
+        }
+        claimableShare = unClaimed * revenuePerToken;
+    }
+
     function claimShare() external {
-        require(claimable, "Claims have not enabled yet.");
+        require(claimable, "Claims is not enabled yet.");
         uint count = balanceOf(msg.sender);
         require(count > 0);
         uint unClaimed;
@@ -125,7 +208,11 @@ contract Gen30 is
             }
         }
         uint balance = unClaimed * revenuePerToken;
-        payable(msg.sender).transfer(balance);
+        console.log("Balance is", balance);
+        console.log("Contract balance is", address(this).balance);
+        require(balance > 0, "You have zero balance");
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Transfer failed.");
     }
 
     function _exists(uint tokenId)
@@ -158,15 +245,72 @@ contract Gen30 is
 
     function setClaimable() external onlyOwner {
         claimable = !claimable;
-        revenuePerToken = totalRevenue / maxSupply;
+        if (claimable) {
+            revenuePerToken = ((totalRevenue * 10) / 100) / maxSupply;
+            ownerCanClaimAll = block.timestamp + 30 days;
+        }
     }
 
     function _setRoyalties(address recipient, uint256 value) internal {
         require(value <= 10000, "Royalty Too high");
-        _royaltyAmount =  uint24(value);
+        _royaltyAmount = uint24(value);
         royaltyOwner = recipient;
     }
 
+    function checkSold(uint[] calldata _tokenIds) external view returns (bool) {
+        for (uint i; i < _tokenIds.length; i++) {
+            if (isSold[_tokenIds[i]]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function unSoldTokens()
+        external
+        view
+        returns (uint[] memory unSoldTokenIds)
+    {
+        uint unSoldCounter;
+        for (uint i; i < maxSupply; i++) {
+            if (!isSold[i]) {
+                unSoldCounter++;
+            }
+        }
+        unSoldTokenIds = new uint[](unSoldCounter);
+        uint counter;
+        for (uint i; i < maxSupply; i++) {
+            if (!isSold[i]) {
+                unSoldTokenIds[counter] = i;
+                counter++;
+            }
+        }
+    }
+
+    function isWhitelisted(address _user, bytes32[] calldata _merkleProof)
+        external
+        view
+        returns (uint8 result)
+    {
+        bytes32 leaf = bytes32(uint256(uint160(_user)));
+        if (MerkleProof.verify(_merkleProof, rootMembers, leaf)) {
+            result = 1;
+        } else if (MerkleProof.verify(_merkleProof, rootFollowers, leaf)) {
+            result = 2;
+        }
+    }
+
+    function withdrawAll() external onlyOwner {
+        console.log("current", block.timestamp);
+        console.log("required", ownerCanClaimAll);
+
+        require(
+            block.timestamp >= ownerCanClaimAll,
+            "Balances are not claimable yet!"
+        );
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Transfer failed.");
+    }
 
     // OVERRIDES
 
@@ -197,5 +341,7 @@ contract Gen30 is
         returns (string memory)
     {
         require(_exists(tokenId), "URI query for nonexistent token");
-        return string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json"));
+        return
+            string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json"));
+    }
 }
---
+++ contracts/WhitelistSale.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract WhitelistSale is ERC721 {
+  bytes32 immutable public merkleRoot;
+  uint256 public nextTokenId;
+  mapping(address => bool) public claimed;
+
+  constructor(bytes32 _merkleRoot) ERC721("ExampleNFT", "NFT") {
+    merkleRoot = _merkleRoot;
+  }
+
+  function toBytes32(address addr) pure internal returns (bytes32) {
+    return bytes32(uint256(uint160(addr)));
+  }
+
+  function mint(bytes32[] calldata merkleProof) public payable {
+    require(claimed[msg.sender] == false, "already claimed");
+    claimed[msg.sender] = true;
+    require(MerkleProof.verify(merkleProof, merkleRoot, toBytes32(msg.sender)) == true, "invalid merkle proof");
+    nextTokenId++;
+    _mint(msg.sender, nextTokenId);
+  }
+}
